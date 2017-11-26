fe481b460563b13713f3283a20ed77efe0eaf1a9
prabesh magar <prabesh3440@gmail.com>
2017-11-26 23:05:17 +0545

    Implemented complex operation for category

--- routes/api.php
+++ routes/api.php
@@ -22,19 +22,29 @@ Route::resource('buyers','Buyer\BuyerController',['only'=> ['index','show']]);
 Route::resource('buyers.transactions','Buyer\BuyerTransactionController',['only'=> ['index']]);
 
 /**buyer product */
-
 Route::resource('buyers.products','Buyer\BuyerProductController',['only'=> ['index']]);
 
 /**buyer seller */
-
 Route::resource('buyers.sellers','Buyer\BuyerSellerController',['only'=> ['index']]);
 
 /**buyer category */
-
 Route::resource('buyers.categories','Buyer\BuyerCategoryController',['only'=> ['index']]);
+
 
 /** Categories*/
 Route::resource('categories','Category\CategoryController',['except'=> ['create','edit']]);
+
+/**category product relation */
+Route::resource('categories.products','Category\CategoryProductController',['only'=> ['index']]);
+
+/**category sellers relation */
+Route::resource('categories.sellers','Category\CategorySellerController',['only'=> ['index']]);
+
+/**category transaction relation */
+Route::resource('categories.transactions','Category\CategoryTransactionController',['only'=> ['index']]);
+
+/**category Buyer relation */
+Route::resource('categories.buyers','Category\CategoryBuyerController',['only'=> ['index']]);
 
 
 /** Product*/
@@ -46,8 +56,10 @@ Route::resource('sellers','Seller\SellerController',['only'=> ['index','show']])
 /** Transactions*/
 Route::resource('transactions','Transaction\TransactionController',['only'=> ['index','show']]);
 
+/** Transactions Category*/
 Route::resource('transactions.categories','Transaction\TransactionCategoryController',['only'=> ['index']]);
 
+/** Transactions Sellers */
 Route::resource('transactions.sellers','Transaction\TransactionSellerController',['only'=> ['index']]);
 
 /** User*/
